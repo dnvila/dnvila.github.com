e5456c7c6289c1127037c74abbd5e1585642c125
dnvila <dnvila@estudiantes.uci.cu>
2012-07-12 12:51:51 -0400

    version con datatable

--- script/app.js
+++ script/app.js
@@ -1,9 +1,36 @@
 /**
  * @author russo and dariel
  */
-YUI().use('node', 'event', function (Y) {
-    // The Node and Event modules are loaded and ready to use.
-    // Your code goes here!
-    Y.one('div#main').setHTML('<p>I am online.</p>');
-    Y.log('I am online');
+YUI().use('node', 'yql','json','datatable','datatable-sort', function(Y){				
+				//query
+			var query = "select * from html where url='http://jlcabrera.pgafan.net/computadoras/pc-de-escritorio/' and xpath='//table[@class=\"adsterix_set\"]//tr//td//a'";
+				Y.YQL(query,function(r){
+					
+					(new Y.DataTable({
+						data: r.query.results.a,
+						columns: [
+							{
+								key:'span',
+								label:'precio',
+								sortable:true
+								
+							},
+							{
+								key:'content',
+								label:'Descripci&oacute;n',
+								sortable:true
+								
+							},
+							{
+								key:'href',
+								label:'Link'
+								
+							}
+						]
+					}).render('body'));
+					
+					Y.one('#loader').remove();
+					
+				});
+
 });
